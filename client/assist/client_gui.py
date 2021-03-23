525dd3ba859814269b9b8a0c3de18dab1c898994
Debagnik <debagnikkar.3@gmail.com>
2021-03-23 14:57:47 +0530

    final size window change

--- client/assist/client_gui.py
+++ client/assist/client_gui.py
@@ -11,7 +11,7 @@ class ChatRoom(object):
     
     def __init__(self):
         self.top = Tk()
-
+        self.top.geometry("700x550")
         self.label = Label(self.top, text='Secure.ly Chat Room v1.2')
         self.label.pack()
 
@@ -19,15 +19,15 @@ class ChatRoom(object):
         self.message_sr = Scrollbar(self.message_show)
         self.message_sr.pack(side=RIGHT, fill=Y)
         self.message_text = Text(self.message_show, height=20, width=50,
-            font=('Courier New', 25))
+            font=('Courier New', 12))
         self.message_text.config(state=DISABLED)
         self.message_sr.config(command=self.message_text.yview)
         self.message_text.pack(side=LEFT, fill=X)
         self.message_show.pack(anchor=N)
 
         self.message_input = Frame(self.top)
-        self.message_ipt_text = Text(self.message_input, height=15, width=47,
-            font=('Courier New', 25))
+        self.message_ipt_text = Text(self.message_input, height=15, width=45,
+            font=('Courier New', 15))
         self.send_msg = Button(self.message_input, text='Send',
                                  command=self.send_message,
                                  fg='white', bg='black')
